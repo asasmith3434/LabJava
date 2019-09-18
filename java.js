7420c70be4b64aa74023af8e7a523abdee88573e
Asa C. Smith <asa.smith@centre.edu>
2019-09-18 15:13:51 -0400

    java

---
+++ java.js
@@ -0,0 +1,11 @@
+
+
+
+var trick=function()
+{
+  var paragraph=document.createElement("p1")
+  console.log("Yes");
+    paragraph.innerText="The Haunted Mansion"
+    document.querySelector("body").append(paragraph);
+    
+}

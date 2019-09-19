d1cddb12803765b896942e5ea54cc94cb9af3fee
Asa C. Smith <asa.smith@centre.edu>
2019-09-18 22:01:44 -0400

    yeet

--- java.js
+++ java.js
@@ -1,11 +1,55 @@
 
 
-
-var trick=function()
+var trick = function()
 {
-  var paragraph=document.createElement("p1")
-  console.log("Yes");
-    paragraph.innerText="The Haunted Mansion"
+var button=document.createElement("button")
+    button.setAttribute("onclick","thot()");
+    button.innerText="The Haunted Mansion"
+    document.querySelector("body").append(button);
+    
+var button=document.createElement("button")
+    button.setAttribute("onclick","yomama()");
+    button.innerText="The Suburban Area"
+    document.querySelector("body").append(button);
+var trick=document.querySelector("#trick")
+trick.setAttribute("onclick",null)
+}
+
+var stay = function()
+{
+var paragraph=document.createElement("p")
+    paragraph.innerText="Watch TV: Story Ends"
     document.querySelector("body").append(paragraph);
+var stay=document.querySelector("#stay")
+    stay.setAttribute("onclick",null)
+}
+
+var thot=function()
+{
+  var paragraph=document.createElement("p")
+  paragraph.innerText="Zombie Eats You: Story Ends"
+    document.querySelector("body").append(paragraph)
+var thot=document.querySelector("#thot")
+stay.setAttribute("onclick",null)
     
 }
+
+var yomama=function()
+{
+    var button=document.createElement("button")
+    button.innerText="You Are Safe"
+    button.setAttribute("onclick","End()")
+    document.querySelector("body").append(button)
+var yomama=document.querySelector("#yomama")
+yomama.setAttribute("onclick",null)
+}
+
+var End=function()
+{
+    var paragraph=document.createElement("p")
+    paragraph.innerText="Yeeet You Get Clapped"
+    document.querySelector("body").append(paragraph)
+var End=document.querySelector("#End")
+End.setAttribute("onclick",null)
+}
+
